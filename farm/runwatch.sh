32e604a5e38fb6fe46f7d7a3c3e63210fd01ccdd
Jean <ivanfarms@avidys.com>
2014-09-27 20:52:54 +0000

    reorg folders

--- farm/runwatch.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-echo "Hello, you are running sass --watch!"
-
-cd 
-
-sudo sass --watch ./MyProjects/farm/css/style.scss:./MyProjects/farm/css/style.css
-
-echo "sass is running, start working."
