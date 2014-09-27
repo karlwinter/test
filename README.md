5828d6c1f1c7d0bb9eb0b700ec92fa748859641c
Jean <ivanfarms@avidys.com>
2014-09-27 19:58:30 +0000

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+site
+====
+
+Front-end (HTML/CSS/JS)
+
+In this branch we store up-to-date development version of the IvanFarms project front-end part.
---
+++ farm/runwatch.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo "Hello, you are running sass --watch!"
+
+cd 
+
+sudo sass --watch ./MyProjects/farm/css/style.scss:./MyProjects/farm/css/style.css
+
+echo "sass is running, start working."
